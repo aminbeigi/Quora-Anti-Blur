f5931a9d8fc2d4fc3bc5a71f42d2dd6b8ed590db
Amin Ghasembeigi <me@aminbeigi.com>
2020-11-19 13:08:55 +1100

    Remove blur

--- src/quora-blur-blocker/content.js
+++ src/quora-blur-blocker/content.js
@@ -1,1 +1,22 @@
-alert("testing")
+
+// globals
+const HREF = 'https://qsbr.fs.quoracdn.net/-4-main.css-27-a63fda2be3e297f3.css';
+const elements = document.querySelectorAll("link[href='" + HREF + "']");
+
+// funcitons
+const removeBlur = async () => {
+    elements.forEach((e) => {
+        e.remove();
+    });
+}
+
+removeBlur();
+
+/*
+const removeBlur = async () => {
+    const f = (e) => {
+        e.remove();
+    }
+    elements.forEach(f);
+}
+*/
